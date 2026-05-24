774f3bfed31c361ddaacb9a85409f271c62c4c70
Jimmy Vilén <jimmy@vilen.nu>
2026-05-24 09:11:24 +0200

    Removed old rails app

--- railsapp/app/models/daily_note.rb
+++
@@ -1,9 +0,0 @@
-class DailyNote < ApplicationRecord
-  self.table_name = "daily_notes"
-
-  belongs_to :user
-
-  validates :date, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/ }
-  validates :content, presence: true
-  validates :date, uniqueness: { scope: :user_id }
-end
--- railsapp/db/migrate/20260423122032_add_project_id_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddProjectIdToTasks < ActiveRecord::Migration[8.1]
-  def change
-    add_reference :tasks, :project, null: true, foreign_key: true
-  end
-end
